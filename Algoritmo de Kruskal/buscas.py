782b8bbab90a069dffd7ff56c0131df0e3853147
Lucas Brito <lucasbsilva29@gmail.com>
2018-09-06 11:05:03 -0300

    Busca em profundidade OK

--- Algoritmo de Kruskal/buscas.py
+++ Algoritmo de Kruskal/buscas.py
@@ -1,23 +1,25 @@
-#!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
+
 def geraLista():
-    n = raw_input("Quantidade de Vertices:")
-    lista=[]
+    n = input('Quantidade de Vertices: ')
+    lista = []
     for i in range(int(n)):
         lista.append([i])
     imprimeLista(lista)
     return lista
 
+
 def imprimeLista(lis):
     for m in lis:
-        print m
+        print (m)
+
 
 def insereAresta(lis):
-    x,y = raw_input("Informe a aresta:").split()
+    x, y = input('Informe a aresta (x,y): ').split(',')
     x = int(x)
     y = int(y)
-    if(x!=y):
+    if (x!=y):
         lis[x].append(y)
         lis[y].append(x)
     else:
@@ -25,86 +27,172 @@ def insereAresta(lis):
 
     imprimeLista(lis)
 
-def verticesAdj(lis,vertice):
-	adj=[]
-	for i in range(1,len(lis[vertice])):
+
+def verticesAdj(lis, vertice):
+	adj = []
+	for i in range(1, len(lis[vertice])):
 		adj.append(lis[vertice][i])
 	return adj
 
-def existeAresta(lis,x,y):
+
+def existeAresta(lis, x, y):
     existe = False
     for i in range(1,len(lis[x])):
-        if lis[x][i]==y:
+        if lis[x][i] == y:
             existe = True
             break
     return existe
 
-def grauVertice(lis,vertice):
-	if existeAresta(lis,vertice,vertice): #verifica se existe laço (obs. apenas 1 laço)
-		return len(verticesAdj(lis,vertice))+1
+
+def grauVertice(lis, vertice):
+	if (lis, vertice, vertice):
+		return len(verticesAdj(lis, vertice)) + 1
 	else:
-		return len(verticesAdj(lis,vertice))
+		return len(verticesAdj(lis, vertice))
+
 
 def buscaLargura(atual):
-	print '\n'
+	print ('\n')
 	visitados = []
 	vizinhos = [atual]
+
 	while vizinhos != '[]':
 		try:
 			visitados.append(int(vizinhos[0]))
-			print '\n'
+			print ('\n')
 
-			adj = verticesAdj(grafo,int(vizinhos.pop(0)))
-
+			adj = verticesAdj(grafo, int(vizinhos.pop(0)))
 			for vertice in adj:
 				if vertice not in visitados and vertice not in vizinhos:
 					vizinhos.append(vertice)
-			print 'Visitados: ', visitados
+			print ('Visitados: ', visitados)
 		except:
-			print '-- FINALIZADO --'
+			print ('-- FINALIZADO --')
 			break
 
+
+def buscaProfundidade(grafo, atual):
+	visitados = []
+	vizinhos = [atual]
+	j = 0
+
+	visitados.append(int(atual))
+
+	while vizinhos != []:
+		try:
+			vizinhos = verticesAdj(grafo, int(visitados[j]))
+
+			i = 0
+			for vizinho in vizinhos:
+				if vizinho not in visitados:
+					atual = vizinho
+					break
+
+				else:
+					i += 1
+
+			if i == len(vizinhos):
+				k = 0
+				for vertice in grafo:
+					if vertice[0] in visitados:
+						k += 1
+						
+					else:
+						m = 1
+						while m < len(visitados):
+							vizinhos_fernando = verticesAdj(grafo, int(visitados[j-m]))
+
+							l = 0
+							find = False
+							for vizinho_fernando in vizinhos_fernando:
+								if vizinho_fernando not in visitados:
+									atual = visitados[j-m]
+									find = True
+									break
+
+								else:
+									l += 1
+
+							if l == len(vizinhos_fernando):
+								m += 1
+
+							if find:
+								break
+
+				if len(grafo) == k:
+					vizinhos = []
+					resultado = visitados
+
+			visitados.append(int(atual))
+			j += 1
+
+		except:
+			print ('--'*45)
+			print ('TRAVOU')
+			break
+
+	return resultado
+
+
 menu = {}
-menu['1']="Criar Lista" 
-menu['2']="Insere Aresta"
-menu['3']="Visualiza Lista"
-menu['4']="Vertices Adjacentes"
-menu['5']="Existe Aresta Entre"
-menu['6']="Grau do Vertice"
-menu['7']="Busca em Largura"
-menu['8']="Sair"
+menu['1'] = 'Criar Lista' 
+menu['2'] = 'Insere Aresta'
+menu['3'] = 'Visualiza Lista'
+menu['4'] = 'Vertices Adjacentes'
+menu['5'] = 'Existe Aresta Entre'
+menu['6'] = 'Grau do Vertice'
+menu['7'] = 'Busca em Largura'
+menu['8'] = 'Samuel'
+menu['9'] = 'Sair'
 grafo = []
+
 while True: 
-	options=menu.keys()
-	options.sort()
-	print '\n'
-	for entry in options:
-		print entry, menu[entry]
-	selection=raw_input("Selecione a opção:")
-	print '\n'
-	if selection =='1':
+	options = menu.keys()
+	options_order = sorted(options)
+	print ('\n')
+	for entry in options_order:
+		print (entry, menu[entry])
+	selection = input('Selecione a opção: ')
+	print ('\n')
+	if selection == '1':
 		grafo = geraLista()
+
 	elif selection == '2':
 		insereAresta(grafo)
+
 	elif selection == '3':
 		imprimeLista(grafo)
-	elif selection == '4':
-		vertice = raw_input("Informe o vertice:")
-		print 'Vertices adjacentes a ',vertice,': ',verticesAdj(grafo,int(vertice))
-	elif selection == '5':
-		v1,v2 = raw_input("Informe os vertices:").split()
-		if existeAresta(grafo,int(v1),int(v2)):
-			print 'Existe!'
-		else:
-			print 'Não existe!'
-	elif selection == '6':
-		vertice = raw_input("Informe o vertice:")
-		print 'Grau do vertice ',vertice,':',grauVertice(grafo,int(vertice))
-	elif selection == '7':
-		atual = raw_input("Digite o vertice inicial: ")
-		buscaLargura(atual)
-	elif selection == '8':
-		break
-	else:
-		print "Opção selecionada INVÁLIDA!" 
 
+	elif selection == '4':
+		vertice = input('Informe o vertice: ')
+		print ('Vertices adjacentes a ', vertice, ': ', verticesAdj(grafo, int(vertice)))
+
+	elif selection == '5':
+		v1, v2 = input('Informe os vertices (v1,v2): ').split(',')
+		if existeAresta(grafo, int(v1), int(v2)):
+			print ('Existe!')
+
+		else:
+			print ('Não existe!')
+
+	elif selection == '6':
+		vertice = input('Informe o vertice: ')
+		print ('Grau do vertice ', vertice, ':', grauVertice(grafo, int(vertice)))
+
+	elif selection == '7':
+		atual = input('Digite o vertice inicial: ')
+		buscaLargura(atual)
+
+	elif selection == '8':
+		atual = input('Digite o vertice inicial: ')
+		atual = int(atual)
+		print ('\n' + '*'*35)
+		print ('CAMINHO PARA BUSCA EM PROFUNDIDADE')
+		print (buscaProfundidade(grafo, atual))
+		print ('*'*35)
+
+	elif selection == '9':
+		break
+
+	else:
+		print ('Opção selecionada INVÁLIDA!')
