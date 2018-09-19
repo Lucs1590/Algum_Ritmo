7c0cca7503a276bcab6a856d680d3b52b2cbbbff
Lucas Brito <lucasbsilva29@gmail.com>
2018-09-18 21:03:30 -0300

    Código Finalizado

--- Recursividade/codigo.py
+++ Recursividade/codigo.py
@@ -22,6 +22,45 @@ def soma(num):
     else:
         return(num + soma(num-1))
 
-print(multiplicacao(2,10))
-print(elevado(2,3))
-print(soma(6))
+
+def prim_alg(num):
+    num = round(num, 2)
+    num_str = str(num)
+    if (len(num_str) == 4):
+        num = (num / 10)
+        num = round(num, 2)
+        num_str = str(num)
+        print(num)
+        if (len(num_str) >= 3):
+            return int(num)
+        else:
+            return int(num*10)
+    else:
+        return (prim_alg(num / 10))
+
+
+def num_lista(L, N):
+    if N not in L:
+        return (L)
+    else:
+        L.remove(N)
+        return (num_lista(L, N))
+
+
+def num_perfeito(num, contador = 1, soma = 0):
+    if (contador == num):
+        return soma == num
+    else:
+        if(num%contador==0): 
+            soma+=contador
+            return num_perfeito(num,contador+1,soma)
+
+
+
+lista = [1,2,3,2,4,5]
+# print(multiplicacao(2,10))
+# print(elevado(2,3))
+# print(soma(6))
+# print(prim_alg(150))
+# print(num_lista(lista,2))
+print(num_perfeito(6))
